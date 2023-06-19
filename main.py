c00e1bdbf9afe15e7232aa2403b9ca05cc60a9b2
Dario Bertolino <dbertolino@DUELL.localdomain>
2023-06-19 18:44:44 +0200

    removing custom log system

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import os
-import logging.config
-
 from fastapi import FastAPI
-
-# setup loggers
-folder_path = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-1])
-logging.config.fileConfig(os.path.join(folder_path, 'logging.conf'), disable_existing_loggers=False)
-
-# get root logger
-logger = logging.getLogger(__name__)  # the __name__ resolve to "main" since we are at the root of the project.
 
 app = FastAPI()
 
