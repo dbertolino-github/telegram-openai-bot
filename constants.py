e900faf8d290cb595cbbd2fa2cbf975154e6d0d8
Dario Bertolino <dbertolino@DUELL.localdomain>
2023-06-20 00:24:17 +0200

    Adding ChatGPT calls

---
+++ constants.py
@@ -0,0 +1,12 @@
+INIT_CHATBOT_PROMPT = """Voglio che ti comporti come il membro del reparto di risorse umane di un'azienda chiamata Digitiamo e il tuo nome è Alf. 
+    Io sono un dipendente che si chiama Tomma e il mio mestiere è quello di Data Scientist all'interno del team di AIKnowYou.
+    Alf:"""
+
+    # Tu mi devi chiedere come prima domanda com'è il mio stato di salute e di benessere nel team di lavoro.  
+    # Durante la conversazione, devi assicurarti di avere una risposta su questi temi: il rapporto con i miei colleghi e il rapporto tra vita personale e lavoro. 
+    # Prima di passare all'argomento successivo, devi chiedere sempre se ho altro da aggiungere all'argomento precedente. 
+    # Se non sai come rispondere, chiedi maggiore contesto per rispondere a ciò che ti viene chiesto.
+    # Prima di chiudere la conversazione, ti devi assicurare se c'è altro di cui io voglio parlare.
+    # Prima di continuare la conversazione, devi aspettare che digito le risposte alle tue domande. 
+    # Tu mi rispondi in maniera empatica e comprensiva. 
+    # Alf:"""
--- main.py
+++ main.py
@@ -2,29 +2,56 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import httpx
 import os
+import openai
+import constants as constants
 
 TOKEN = "5764835537:AAEVeCtTnGPtfsupAh-mJF08ajoYJY6EI2I"
-BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
+BASE_URL_TELEGRAM = f"https://api.telegram.org/bot{TOKEN}"
+BASE_URL_SLACK = "https://hooks.slack.com/services/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"
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 client = httpx.AsyncClient()
+
+# CUSTOM MESSAGES
+def get_chatgpt_response(messages, model="gpt-3.5-turbo"):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages)
+
+    return response['choices'][0]['message']['content']
+
+def get_initial_message():
+    messages=[{"role": "system", "content": constants.INIT_CHATBOT_PROMPT}]
+    return messages
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
+
     return {"message": "Hello World. Welcome to FastAPI!"}
 
-
-@app.post("/webhook/")
+@app.post("/telegram")
 async def webhook(req: Request):
+
     data = await req.json()
     chat_id = data['message']['chat']['id']
     text = data['message']['text']
+    response = "SOMETHING WENT WRONG"
+
+    if text == "/start":
+        response = get_initial_message()
+    else:
+        response = get_chatgpt_response([text])
+
+    await client.get(f"{BASE_URL_TELEGRAM}/sendMessage?chat_id={chat_id}&text={response}")
+
+    return data
+
+@app.post("/slack")
+async def webhook(req: Request):
 
     # TODO: chiamate a OPENAI
 
-    response = "sti cazzi non ho l'intelligenza artificiosa"
+    return "yolo"
 
-    await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={response}")
-
-    return data
